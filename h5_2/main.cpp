736e2cf0c193d34df41dd7c0c8f4312984fa676e
Mariia <mariia@Mariias-MacBook-Air.local>
2025-12-05 08:30:24 +0200

    Tehtävä 5 osa 2

---
+++ h5_2/main.cpp
@@ -0,0 +1,28 @@
+#include "classa1.h"
+#include "classa2.h"
+#include "classb.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ClassB objB;
+    objB.setInfo("Olion B asettama info");
+
+    ClassA1 objA1(objB);
+    objA1.setBinfo("Olion objA1 asettama info");
+
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA1: "<<objA1.getBinfo()<<endl;
+
+    ClassB &refB=objB;
+    ClassA2 objA2(refB);
+    objA2.setBinfo("Olion Agr asettama info");
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA2 "<<objA2.getBinfo()<<endl;
+    cout<<endl;
+
+    return 0;
+}
